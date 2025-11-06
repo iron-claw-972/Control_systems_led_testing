8f1a0294310fc5624e1b066988ad35e2e51bc1fe
aarav-gupty <ezpressureco@gmail.com>
2025-11-05 18:24:21 -0800

    Added new repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# FRC2025
+
+[![CI](https://github.com/iron-claw-972/FRC2025/actions/workflows/main.yml/badge.svg)](https://github.com/iron-claw-972/FRC2025/actions/workflows/main.yml)
+
+Code for the 2024-2025 FRC Season.
---
+++ src/main/java/frc/robot/subsystems/PhotoelectricSensor/PhotoelectricSensor.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.PhotoelectricSensor;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// UNABLE TO WORK: Reason is because of hardware as the voltage drop is not significant enough
+public class PhotoelectricSensor extends SubsystemBase {
+    private final DigitalInput inputPin;
+
+    public PhotoelectricSensor(int channel) {
+        inputPin = new DigitalInput(channel);
+    }
+
+    @Override
+    public void periodic() {
+        boolean status = inputPin.get();
+
+        if (!status) {
+            // grounded
+            System.out.println("GROUNDED");
+        } else {
+            // not grounded
+            System.out.println("NOT GROUNDED");
+        }
+    }
+}
